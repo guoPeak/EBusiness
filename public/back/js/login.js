8a7aa37ddbcb23fc36617fb40959b610d6023e26
guoyaofeng <guo_peak@163.com>
2018-06-17 14:22:06 +0800

    项目初始化

---
+++ public/back/js/login.js
@@ -0,0 +1,55 @@
+
+$(function () {
+
+    $('form').bootstrapValidator({
+
+        //1. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        fields: {
+            username: {
+               validators: {
+                   notEmpty: {
+                       message: '用户名不能为空'
+                   },
+                   stringLength: {
+                       min: 4,
+                       max: 12,
+                       message: '用户名长度必须在4到12之间'
+                   }
+               } 
+            },
+
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: '密码长度必须在6到12之间'
+                    }
+                } 
+             }
+        }
+    });
+
+    $('form').on('success.form.bv', function (e) {  
+        e.preventDefault();
+        
+        $.ajax({
+            type: "post",
+            url: "url",
+            data: "data",
+            success: function (response) {
+                
+            }
+        });
+    })
+
+});
