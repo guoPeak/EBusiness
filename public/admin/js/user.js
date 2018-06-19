e38d2fb6a09cc10ab361655d9dc5410e394756c7
guoyaofeng <guo_peak@163.com>
2018-06-19 12:46:01 +0800

    用户管理功能完成

---
+++ public/admin/js/user.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    var page = 1;
+    var pageSize = 6;
+
+    render();
+
+    //注册启用或禁用事件
+    $('tbody').on('click', '.btn', function () {
+        var id = $(this).parent().data('id');
+        var isDelete = $(this).hasClass('btn-success') ? 1 : 0;
+        // console.log(id, isDelete);
+        $.ajax({
+            type: "post",
+            url: "/user/updateUser",
+            data: {
+                id: id,
+                isDelete: isDelete
+            },
+            success: function (info) {
+                if (info.success) {
+                    render();
+                }
+            }
+        });
+    });
+
+
+    function render() {
+        $.ajax({
+            type: "get",
+            url: "/user/queryUser",
+            data: {
+                page: page,
+                pageSize: pageSize
+            },
+            success: function (info) {
+                // console.log(info);
+                var html = template('tpl', info);
+                $('tbody').html(html);
+
+                //添加分页功能
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: page,
+                    totalPages: Math.ceil(info.total / info.size),
+                    onPageClicked: function (a, b, c, p) {
+                        page = p;
+                        render();
+                    }
+                })
+            }
+        });
+    }
+
+})
